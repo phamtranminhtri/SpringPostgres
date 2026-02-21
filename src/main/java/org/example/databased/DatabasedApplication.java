96e9dee818b74628f641e46c92a8886cdfd665cc
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-02-21 11:09:42 +0700

    Initial commit

---
+++ src/main/java/org/example/databased/DatabasedApplication.java
@@ -0,0 +1,13 @@
+package org.example.databased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/databased/DatabasedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.databased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
